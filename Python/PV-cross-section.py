74ca829b230e5877686531c607e402d886aa77be
MethaneRain <justin.richling@gmail.com>
2020-01-20 17:09:21 +0000

    Create PV-cross-section.py

---
+++ Python/PV-cross-section.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 20 17:08:18 2020
+
+@author: chowdahead
+"""
+
+from metpy.plots import ctables
+mycmap = ctables.registry.get_colortable("NWSReflectivity")
