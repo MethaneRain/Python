8fe3d27dc3ba46ba15e9d4e1ec2be36e7a786795
MethaneRain <justin.richling@gmail.com>
2020-04-08 00:18:08 +0000

    Update test_new_netcdf.py

--- netcdf4/test_new_netcdf.py
+++ netcdf4/test_new_netcdf.py
@@ -10,7 +10,34 @@ Created on Wed Apr  8 00:11:32 2020
 #-----------------------------------------------------------------------------#
 import netCDF4 as nc
 import numpy as np
-new_ds = nc.Dataset("new_nectdf_file_example.nc")
+new_ds = nc.Dataset("test.nc")
 
 print(list(new_ds.variables))
 
+# Build our backgroud map and plotting control
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+
+# Needed for plot stuff
+import matplotlib.pyplot as plt
+
+# Set pur lat/lon box for plotting 
+#extent = [-125,-70,20,50]
+extent = [-125,-70,25,48]
+
+# Set projection of data
+datacrs = ccrs.PlateCarree()
+
+# Set projection of map
+plotcrs = ccrs.LambertConformal()
+
+# Add Map Features
+country_borders = cfeature.NaturalEarthFeature(category='cultural',
+    name='admin_0_countries',scale='50m', facecolor='none')
+
+state_boundaries = cfeature.NaturalEarthFeature(category='cultural',
+        name='admin_1_states_provinces_lakes', scale='50m', facecolor='none')
+
+press = new_ds.variables['pres'][:]
+Lon = new_ds.variables['lons'][:]
+Lat = new_ds.variables['lats'][:]
