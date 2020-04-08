300d7e01f049db3d1a1d957145dcbf7348551802
MethaneRain <justin.richling@gmail.com>
2020-04-08 00:02:52 +0000

    Update new_nectdf_file_example.py

--- netcdf4/new_nectdf_file_example.py
+++ netcdf4/new_nectdf_file_example.py
@@ -7,6 +7,7 @@ Created on Tue Apr  7 23:55:33 2020
 """
 import netcdf4 as nc
 import numpy as np
+from numpy import dtype
 
 ds = nc.Dataset("test.nc","w",format="NETCDF4")
 
@@ -16,21 +17,21 @@ lat_vals = np.arange(-90,92.5,2.5) # This will give 73 latitudes, every 2.5 degr
 ds.createDimension('lat', len(lat_vals))
 ds.createDimension('lon', len(lon_vals))
 
-lat = ncout.createVariable('lats', dtype('double').char, ('lat'))
+lat = ds.createVariable('lats', dtype('double').char, ('lat'))
 lat.standard_name = 'latitude'
 lat.long_name = 'latitude'
 lat.units = 'degrees_north'
 lat.axis = 'Y'
 
 # create longitude axis
-lon = ncout.createVariable('lons', dtype('double').char, ('lon'))
+lon = ds.createVariable('lons', dtype('double').char, ('lon'))
 lon.standard_name = 'longitude'
 lon.long_name = 'longitude'
 lon.units = 'degrees_east'
 lon.axis = 'X'
 
 # create (made-up) pressure axis
-press = ncout.createVariable('pres', dtype('double').char, ('lat', 'lon'))
+press = ds.createVariable('pres', dtype('double').char, ('lat', 'lon'))
 press.standard_name = 'pressure'
 press.long_name = 'Made-Up Pressure'
 press.units = 'hPa'
