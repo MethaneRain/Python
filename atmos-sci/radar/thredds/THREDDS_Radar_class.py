04dbb1da39cd46c9b43a934a2ee5970d6ad30206
MethaneRain <justin.richling@gmail.com>
2020-05-02 23:03:32 +0000

    Update THREDDS_Radar_class.py

--- atmos-sci/radar/thredds/THREDDS_Radar_class.py
+++ atmos-sci/radar/thredds/THREDDS_Radar_class.py
@@ -12,4 +12,32 @@ class THREDDS_Radar:
     def __init__(self,author):
         self.author=author
         
+    def get_radar_data(rs,station,product,dataset_name,start,end):
+        import numpy as np
+        query = rs.query()
+        query.stations(station).time_range(start,end).variables(product)
+        print(end)
+        filetime = "{0:%Y_%m_%d_%H%MZ}".format(end)
+        query_cat = rs.get_catalog(query)
+        data = query_cat.datasets[0].remote_access()
+        
     
+        range_data = data.variables['gate'][:]
+        azimuth_data = data.variables['azimuth'][:]
+        radar_data = data.variables[dataset_name][:]
+    
+        x = range_data*np.sin(np.deg2rad(azimuth_data))[:,None]
+        y = range_data*np.cos(np.deg2rad(azimuth_data))[:,None]
+        
+        return data,range_data,azimuth_data,radar_data,x,y,filetime 
+    
+#class Meteorologist:
+#    education = "Atmospheric Science"#
+#
+#    def __init__(self,name,age,employer):
+#        self.name = name
+#        self.age = age
+#        self.employer = employer
+
+#    def description(self):
+#        return f"Hi, my name is {self.name} and I'm {self.age} years old and work for {self.employer}!"
