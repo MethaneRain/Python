46e1d2990a5d9d18cf0ffbec1678242013a4a153
MethaneRain <justin.richling@gmail.com>
2020-06-07 00:57:08 +0000

    Create UWyoSounding.py
    Class for scraping and plotting "complex" soundings from UWyo data and MetPy SkewT

---
+++ atmos-sci/sounding/UWyoSounding.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  7 00:52:49 2020
+
+@author: Justin Richling
+
+University of Wyoming and MetPy Soundings (non Siphon)
+"""
+
+class UWyoSound:
+    
+    def __init__(self):
+        '''
+        Default values
+        
+        * date -> current date
+        * initialization hour -> 0000Z
+        * image save path -> cwd
+        * countour levels -> 100
+        * extent -> CONUS
+        * contour color -> black
+        '''
+        from datetime import datetime
+        now = datetime.utcnow()
+        self.today_day = int('{0:%d}'.format(now))
+        self.today_year = int('{0:%Y}'.format(now))
+        self.today_month = int('{0:%m}'.format(now))
+        print(self.today_day,self.today_year,self.today_month)
+        
+        # initialization hour for model forecast
+        self.launch_hour = "12"
+        
+        # current working drive for saved images
+        self.im_save_path = "./"
+        
+        self.station_num = 72469
+        self.station_name = "DNR"
+        
+        
